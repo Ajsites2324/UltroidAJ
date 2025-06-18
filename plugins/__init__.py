771094242f2cdb9b1b2aaf5ceb0048d84be06d32
Ajsites2324 <97234991+Ajsites2324@users.noreply.github.com>
2025-06-18 15:50:12 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -67,7 +67,7 @@ STUFF = {}
 # Feel Free to Add Any other...
 
 NOSPAM_CHAT = [
-    -1001361294038,  # UltroidSupportChat
+    -1004815737809,  # UltroidSupportChat
     -1001387666944,  # PyrogramChat
     -1001109500936,  # TelethonChat
     -1001050982793,  # Python
